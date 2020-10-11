ded90cf558c2df47ed2de994586fbba30d092ecc
Stefan Grandner <sgrandner@gmx.de>
2020-10-11 17:01:02 +0200

    initial spring boot application

---
+++ src/main/java/com/digitalbookshelf/librarycatalog/LibraryCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.digitalbookshelf.librarycatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCatalogApplication.class, args);
+	}
+
+}
